bfe177ff5a984db8a3a26f82ed4dfcb068588942
Go-Jiwoo <giwoolove@naver.com>
2023-08-01 15:35:09 +0900

    230801-1

--- css/js.js
+++ css/js.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $("#btn").click(function () {
         $("*").toggleClass("dark");
         if ($(this).hasClass("dark")) {
-            $("#btn img").attr("src", "img/dark.png");
+            $("#btn img").attr("src", "img/button/dark.png");
             $("div.video div span").css("color", "#ffffff");
-            $("#btn2 img").attr("src", "img/searchlight.svg");
+            $("#btn2 img").attr("src", "img/button/searchlight.svg");
             $("html, #btn2 button, #btn2 input, .player").css("background-color", "rgb(34,34,34)");
             $("div.ChartIn .chart").hover(function () {
                 $(this).css("background", "#111111");
@@ -14,9 +14,9 @@ $(document).ready(function () {
             });
 
         } else {
-            $("#btn img").attr("src", "img/light.png");
+            $("#btn img").attr("src", "img/button/light.png");
             $("div.video div span").css("color", "#00CD3C");
-            $("#btn2 img").attr("src", "img/search.svg");
+            $("#btn2 img").attr("src", "img/button/search.svg");
             $("html ,#btn2 button, #btn2 input, .player").css("background-color", "#ffffff");
             $("div.ChartIn .chart").hover(function () {
                 $(this).css("background", "#f0f0f0");
@@ -36,12 +36,12 @@ $(document).ready(function () {
     $(window).resize(function () {
         if ($("#Main_Page").width() <= 1099) {
             if ($("#Main_Page").width() <= 700) {
-                $("#wrap4 img").attr("src", "img/muwa2.svg");
+                $("#wrap4 img").attr("src", "img/musicwave/muwa2.svg");
             } else {
-                $("#wrap4 img").attr("src", "img/muwa3.svg");
+                $("#wrap4 img").attr("src", "img/musicwave/muwa3.svg");
             }
         } else {
-            $("#wrap4 img").attr("src", "img/muwa.svg");
+            $("#wrap4 img").attr("src", "img/musicwave/muwa.svg");
         }
 
     });
@@ -53,6 +53,10 @@ $(document).ready(function () {
             $("#singer").text($(this).prev().attr("singer"));
         });
 
+    });
+    
+    $(".controler img:nth-child(2)").click(function(){
+       $(this).toggleClass("select1"); 
     });
 
 
