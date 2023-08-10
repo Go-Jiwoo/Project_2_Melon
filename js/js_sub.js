c1d204117c621c591e4206ee2c4e8160331ab19f
Go-Jiwoo <giwoolove@naver.com>
2023-08-10 17:41:59 +0900

    230810-1

--- js/js_sub.js
+++ js/js_sub.js
@@ -3,14 +3,21 @@ $(document).ready(function () {
     // 선택된 UL은 변형 없도록 고정
     $(".collect > li").mouseover(function () {
         if ($(this).hasClass("selected")) {
-            $(this).addClass("hover")}
-    }).mouseleave(function(){
+            $(this).addClass("hover")
+        }
+    }).mouseleave(function () {
         $(this).removeClass("hover")
     });
 
+
     // 클릭하면 class 변경
-    $(".collect > li").click(function(){
+    $(".collect > li").click(function () {
         $(this).addClass("selected").siblings().removeClass("selected");
+
+        let a = $(this).index();
+        $(".group").eq(a).show();
+        $(".group").eq(a).siblings().hide();
     });
-    
+
+
 });
